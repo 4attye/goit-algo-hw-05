253ce5b09f344591df9b7d8a3c5c7a09bef4dbb1
4attye <167115532+4attye@users.noreply.github.com>
2024-07-11 21:25:04 +0300

    Homework

--- cash.py
+++ cash.py
@@ -1,10 +1,8 @@
 
 def caching_fibonacci():
-    cache = {}
+    cache = {0:0, 1:1}
     def fibonacci(n):
-        if n <= 0: return 0
-        elif n == 1: return 1
-        elif n in cache: return cache[n]
+        if n in cache: return cache[n]
         else:
             cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
             return cache[n]
